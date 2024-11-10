94064b4dee40680b9d486ff51aa0582fa636943c
Eegii219 <Enkhtsetseg.Unurbayar@tachconsulting.tech>
2024-11-10 17:19:11 -0500

    initial commit

---
+++ fFrst-Ui-Build/Frst-Ui-Build/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  fFrst-Ui-Build
+//
+//  Created by Enkhtsetseg Unurbayar on 11/10/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
